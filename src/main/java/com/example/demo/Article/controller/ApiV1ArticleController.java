7d28c440666885cd374251dfec8bebb262b635f8
jinainkorea <kimjina1152@gmail.com>
2024-10-29 12:24:09 +0900

    스웨거 사용

--- src/main/java/com/example/demo/Article/controller/ApiV1ArticleController.java
+++ src/main/java/com/example/demo/Article/controller/ApiV1ArticleController.java
@@ -6,24 +6,31 @@ import com.example.demo.Article.request.ArticleModifyRequest;
 import com.example.demo.Article.service.ArticleService;
 import com.example.demo.Article.request.ArticleCreateRequest;
 import com.example.demo.global.RsData.RsData;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/articles")
+@RequestMapping(value = "/api/v1/articles", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+@Tag(name="ApiV1Controller", description = "api")
 public class ApiV1ArticleController {
     private final ArticleService articleService;
 
     @GetMapping("")
+    @Operation(summary = "게시글 다건 조회")
     public RsData<List<ArticleDTO>> list() {
         return RsData.of("200", "다건 조회 성공", this.articleService.list());
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "게시글 단건 조회")
     public RsData<ArticleDTO> getArticle(@PathVariable("id") Long id) {
         ArticleDTO articleDTO = this.articleService.getArticleDTO(id);
 
@@ -31,6 +38,7 @@ public class ApiV1ArticleController {
     }
 
     @PostMapping("")
+    @Operation(summary = "게시글 생성")
     public RsData create(@Valid @RequestBody ArticleCreateRequest articleCreateRequest) {
         Article article = new Article(articleCreateRequest.getSubject(), articleCreateRequest.getContent());
         ArticleDTO articleDTO = new ArticleDTO(article);
@@ -39,6 +47,7 @@ public class ApiV1ArticleController {
     }
 
     @PatchMapping("/{id}") // 특정 ID를 사용하여 수정
+    @Operation(summary = "게시글 수정")
     public RsData<ArticleDTO> modify(@PathVariable Long id,
                                      @Valid @RequestBody ArticleModifyRequest articleModifyRequest) {
         Article article = this.articleService.getArticle(id);
@@ -51,6 +60,7 @@ public class ApiV1ArticleController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "게시글 삭제")
     public RsData<Object> delete(@PathVariable("id") Long id) {
         this.articleService.delete(this.articleService.getArticle(id));
         return RsData.of("200", "삭제 성공", this.list());
--- src/main/java/com/example/demo/Article/request/ArticleCreateRequest.java
+++ src/main/java/com/example/demo/Article/request/ArticleCreateRequest.java
@@ -1,5 +1,6 @@
 package com.example.demo.Article.request;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
